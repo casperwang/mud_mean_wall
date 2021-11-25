5774683ef2f8dac7c72275b114ceb24df244fe0c
ericxiaogit <41057527+ericxiaogit@users.noreply.github.com>
2021-11-25 10:49:59 +0800

    add floorsum, discretelog

---
+++ 7_Polynomial/r.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void swap(int *a, int *b)
+{
+
+
+
+
+ int tmp = *a;
+
+  *a = *b;
+
+  *b = tmp;
+
+}
+int main()
+{
+  int a,b;
+  scanf("%d%d",&a,&b);
+  swap(&a,&b);
+  printf("%d %d\n",a,b);
+}
